128b243882ee4c052b6acc12ea0d095804587fbc
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-07 10:32:10 -0300

    feat(styles): add font Inter default in App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import font from "@/styles/font";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={font.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
