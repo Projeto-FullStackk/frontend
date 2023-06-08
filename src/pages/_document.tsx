7bdf9c2f7168510136b2cdca3e8c6fc65d10f157
Milena <milenarodarte@users.noreply.github.com>
2023-06-08 12:54:28 -0300

    feat: navbar desktop

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Navbar from "@/components/Navbar";
 const Document = () => {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Navbar
+          screenType="desktop"
+          navbarType="aut"
+          inicials="NP"
+          name="Nome Pessoa"
+        />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
 
 export default Document;
