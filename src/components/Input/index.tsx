d42422de82fefe14a8d4c96eadd9440665976441
Gyovanna Almeida <gyovanna_almeida@hotmail.com>
2023-06-09 16:03:11 -0300

    feat: create input components

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,33 @@
+interface iInputProps {
+  labelName: string;
+  name: string;
+  id: string;
+  placeholder: string;
+}
+
+export const Input = ({ labelName, name, id, placeholder }: iInputProps) => {
+  return (
+    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+      <div className="sm:col-span-4">
+        <label
+          htmlFor="username"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          {labelName}
+        </label>
+        <div className="mt-2">
+          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            <input
+              type="text"
+              name={name}
+              id={id}
+              autoComplete="username"
+              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+              placeholder={placeholder}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components";
+import { Input } from "@/components/Input";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Input labelName="test" name="test" id="test" placeholder="test" />
     </>
-  )
-}
+  );
+};
 
 export default Home;
