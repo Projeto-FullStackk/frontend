361253a85e9b3e659f5d3b41247e9b5f7753e38e
Gyovanna <gyovanna_almeida@hotmail.com>
2023-06-15 14:40:36 -0400

    feat: create auth context

---
+++ src/schemas/users.schema.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().nonempty("Campo obrigatório"),
+});
+
+export const userSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  cpf: z.string().length(11),
+  cellphone: z.string().length(12),
+  birthdate: z.string().length(6),
+  description: z.string(),
+  cep: z.string().length(8),
+  state: z.string(),
+  city: z.string(),
+  street: z.string(),
+  number: z.string(),
+  confirmPass: z.string(),
+});
+
+export type UserData = z.infer<typeof userSchema>;
+export type LoginData = z.infer<typeof loginSchema>;
