2590b604daf7b3ff200a04e08a25d66fc453dac3
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-15 16:57:21 -0300

    fix(components): refact Input component

--- src/components/Input/TextareaForm/index.tsx
+++ src/components/Input/TextareaForm/index.tsx
@@ -1,15 +1,16 @@
-import { iInputProps } from "..";
+import { iTextareaFormProps } from "../interfaces";
 
-type iTextareaFormProps = Pick<
-  iInputProps,
-  "id" | "placeholder" | "register"
->
-
-const TextareaForm = ({ id, placeholder, register }: iTextareaFormProps) => {
+const TextareaForm = ({
+  id,
+  disabled,
+  placeholder,
+  register,
+}: iTextareaFormProps) => {
   return (
     <textarea
       id={id}
       placeholder={placeholder}
+      disabled={disabled}
       autoComplete="off"
       {...register}
       className="w-full h-20 px-4 py-3 border-2 border-gray-7 rounded text-base text-gray-1 outline-none resize-none transition-colors placeholder-shown:text-gray-3 hover:bg-gray-8 hover:border-gray-7 focus:bg-white focus:border-brand-1"
