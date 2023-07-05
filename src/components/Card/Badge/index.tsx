65cd056a8dc621fc34e0c98bce60af8e238ccf52
Luan Florencio <luanflorencio.dev@gmail.com>
2023-07-04 23:05:44 -0300

    feat: add badge message

--- src/components/Card/Badge/index.tsx
+++ src/components/Card/Badge/index.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const Badge = () => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleMessage = () => setShowMessage(!showMessage);
+
   return (
-    <span className="w-max h-max p-1 bg-[#349974] rounded-sm font-medium text-sm text-white absolute top-0 right-0">
-      $
+    <span
+      onMouseOver={handleMessage}
+      onMouseOut={handleMessage}
+      className="w-max h-max p-1 bg-[#349974] rounded-sm font-medium text-sm text-white absolute top-0 right-0"
+    >
+      {showMessage ? "Preço vantajoso!" : "$"}
     </span>
   )
 }
